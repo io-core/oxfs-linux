b20db77675046efe61600d362248a8af4b5eb961
Charles Perkins <charlesap@gmail.com>
2020-07-17 13:19:52 -0700

    add command line options to oxfstool

--- oxfsgo/cmd/oxfstool.go
+++ oxfsgo/cmd/oxfstool.go
@@ -2,11 +2,30 @@ package main
 
 import (
   "fmt"
+  "flag"
 
   "github.com/io-core/oxfs-linux/oxfsgo"
 )
 
 func main() {
-  fmt.Println("starting oxfstool")
-  fmt.Println("Config:", oxfsgo.Config())
+
+        inPtr := flag.String("i", "RISC.img", "Disk image to read")
+        outPtr := flag.String("o", "OXFS.img", "Disk image to write")
+	convPtr := flag.Bool("convert", false, "Convert a disk image")	
+        checkPtr := flag.Bool("check", false, "Check a disk image")
+
+	flag.Parse()
+
+	fmt.Println("starting oxfstool")
+	fmt.Println("Config:", oxfsgo.Config())
+	if *convPtr {
+	        fmt.Println("converting")
+	        fmt.Println("From:", *inPtr)
+	        fmt.Println("To:", *outPtr)
+	}else if *checkPtr{
+                fmt.Println("Checking:", *inPtr)
+	}else{
+                fmt.Println("Must specify convert or check")
+	}
+
 }

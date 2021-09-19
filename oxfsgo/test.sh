95df31006bd09841a77af28dd635719e1d352fe2
Charles Perkins <chuck@kuracali.com>
2021-09-19 08:56:48 -0700

    add tests

--- oxfsgo/test.sh
+++ oxfsgo/test.sh
@@ -4,18 +4,30 @@ unzip RISCimg.zip
 
 echo "oxfstool version " `./oxfstool -V`
 
-echo "tool directory:"
-ls -alh
-
+#test expanding an unpadded image
 mkdir -p onp
 ./oxfstool -o2f -i io.img -o onp
 
-echo "expanded not padded image:"
-ls -alh onp
+#echo "expanded not padded image:"
+#ls -alh onp
 
+./oxfstool -f2o -i onp -i io2.img -s 8M
+
+#test creating an unpadded image
+mkdir -p onp2
+./oxfstool -o2f -i io2.img -o onp2
+
+#round-trip test
+diff -r onp onp2
+
+#test expanding a padded image
 mkdir -p op
 ./oxfstool -o2f -i RISC.img -o op
 
-echo "expanded padded image:"
-ls -alh op
+#echo "expanded padded image:"
+#ls -alh op
+
+echo "tool directory:"
+ls -alh
+
 

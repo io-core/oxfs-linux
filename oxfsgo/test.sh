cb856d5cb52050559d44a58839d41898210852c2
Charles Perkins <chuck@kuracali.com>
2021-09-19 08:34:31 -0700

    expand tests

--- oxfsgo/test.sh
+++ oxfsgo/test.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-TOOL=$1
-
 unzip RISCimg.zip
-./$TOOL -V
+
+echo "oxfstool version " `./oxfstool -V`
+
+echo "tool directory:"
+ls -alh
+
+mkdir -p onp
+./oxfstool o2f -i io.img -o onp
+
+echo "expanded not padded image:"
+ls -alh 
+
+mkdir -p op
+./oxfstool o2f -i RISC.img -o op
+
+echo "expanded padded image:"
+ls -alh
+

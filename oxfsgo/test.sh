587645f7ad49f211e035d9290629a6139391a80b
Charles Perkins <chuck@kuracali.com>
2021-09-19 08:44:11 -0700

    adjust parameters

--- oxfsgo/test.sh
+++ oxfsgo/test.sh
@@ -8,14 +8,14 @@ echo "tool directory:"
 ls -alh
 
 mkdir -p onp
-./oxfstool o2f -i io.img -o onp
+./oxfstool -o2f -i io.img -o onp
 
 echo "expanded not padded image:"
-ls -alh 
+ls -alh onp
 
 mkdir -p op
-./oxfstool o2f -i RISC.img -o op
+./oxfstool -o2f -i RISC.img -o op
 
 echo "expanded padded image:"
-ls -alh
+ls -alh op
 
